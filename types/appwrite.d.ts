4020785c45c536b1fca53b48ac3257e97973a9de
nathfavour <116535483+nathfavour@users.noreply.github.com>
2026-05-31 15:26:26 +0000

    🎨 refactor: remove optional source property from types
    Eliminate the optional `source` property from various type definitions in `appwrite.d.ts` to streamline the model structure and improve clarity. This change enhances type consistency across models.

--- types/appwrite.d.ts
+++ types/appwrite.d.ts
@@ -201,7 +201,6 @@ export type Notes = Models.Row & {
     isFile: boolean;
     isTotp: boolean;
     isDiscussion: boolean;
-    source?: string | null;
 }
 
 export type Comments = Models.Row & {
@@ -360,7 +359,6 @@ export type Credentials = Models.Row & {
     isGuest: boolean | null;
     isShared: boolean | null;
     isPinned: boolean | null;
-    source?: string | null;
 }
 
 export type Identities = Models.Row & {
@@ -435,7 +433,6 @@ export type TotpSecrets = Models.Row & {
     isGuest: boolean | null;
     isShared: boolean | null;
     isPinned: boolean | null;
-    source?: string | null;
 }
 
 export type Keychain = Models.Row & {
@@ -828,7 +825,6 @@ export type Events = Models.Row & {
     isPublic: boolean | null;
     isGuest: boolean | null;
     isPinned: boolean | null;
-    source?: string | null;
 }
 
 export type Calendars = Models.Row & {
@@ -856,7 +852,6 @@ export type Tasks = Models.Row & {
     isPublic: boolean | null;
     isGuest: boolean | null;
     isPinned: boolean | null;
-    source?: string | null;
 }
 
 export type Forms = Models.Row & {
@@ -870,7 +865,6 @@ export type Forms = Models.Row & {
     isPublic: boolean | null;
     isGuest: boolean | null;
     isPinned: boolean | null;
-    source?: string | null;
 }
 
 export type FormSubmissions = Models.Row & {
